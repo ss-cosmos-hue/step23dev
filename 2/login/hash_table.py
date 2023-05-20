30cce0331cba08170159c68dfc021c3f983644f3
ss-cosmos-hue <122190026+ss-cosmos-hue@users.noreply.github.com>
2023-05-20 15:46:21 +0900

    implemented rehash

--- 2/login/hash_table.py
+++ 2/login/hash_table.py
@@ -8,12 +8,12 @@ import random, sys, time
 # The goal is to implement the data structure yourself.                   #
 #                                                                         #
 ###########################################################################
-
+NONEED = -1
 # Hash function.
 #
 # |key|: string
 # Return value: a hash value
-def calculate_hash(key):
+def calculate_hash(key):#10桁149種類(素数)を想定
     assert type(key) == str
     # Note: This is not a good hash function. Do you see why?
     hash = 0
@@ -45,10 +45,10 @@ class Item:
 class HashTable:
 
     # Initialize the hash table.
-    def __init__(self):
+    def __init__(self,bucket_size = 97):
         # Set the initial bucket size to 97. A prime number is chosen to reduce
         # hash conflicts.
-        self.bucket_size = 97
+        self.bucket_size = bucket_size
         self.buckets = [None] * self.bucket_size
         self.item_count = 0
 
@@ -59,7 +59,7 @@ class HashTable:
     # |value|: The value of the item.
     # Return value: True if a new item is added. False if the key already exists
     #               and the value is updated.
-    def put(self, key, value):
+    def put(self, key, value,under_reconstruction = False):
         assert type(key) == str
         self.check_size() # Note: Don't remove this code.
         bucket_index = calculate_hash(key) % self.bucket_size
@@ -71,7 +71,9 @@ class HashTable:
             item = item.next
         new_item = Item(key, value, self.buckets[bucket_index])#古い連結リストが，新しいアイテムにぶら下がる
         self.buckets[bucket_index] = new_item
-        self.item_count += 1
+        if not under_reconstruction:#under_reconstructionの場合はitem_countがすでにupdateされている
+            self.item_count += 1
+            self.reconstruct_if_need()
         return True
 
     # Get an item from the hash table.
@@ -109,6 +111,7 @@ class HashTable:
                 else:
                     pre_item.next = item.next 
                 self.item_count -= 1
+                self.reconstruct_if_need()
                 return True 
             pre_item = item #参照わたしになるか?
             item = item.next
@@ -117,7 +120,6 @@ class HashTable:
 
     def print_table(self):#for debugging
         for (i,bucket) in enumerate(self.buckets):
-            # print("bucket")
             item = bucket
             while item:
                 print("bucket:",i,item.key,item.value)
@@ -134,8 +136,31 @@ class HashTable:
     #
     # Note: Don't change this function.
     def check_size(self):
-        assert (self.bucket_size < 100 or
-                self.item_count >= self.bucket_size * 0.3)
+        assert (self.bucket_size < 100 or self.item_count >= self.bucket_size * 0.3)
+         
+    def reconstruct_if_need(self): 
+        new_bucket_size = self.design_new_table()
+        if  new_bucket_size != NONEED:
+            print("under_reconstruction_to" ,new_bucket_size)
+            new_table = HashTable(new_bucket_size)
+            new_table.item_count =  self.item_count
+            for cur_bucket in self.buckets:
+                item = cur_bucket 
+                while item:
+                    new_table.put(item.key,item.value,under_reconstruction=True)
+                    item = item.next
+            self.bucket_size = new_bucket_size 
+            self.buckets = new_table.buckets 
+            self.item_count = new_table.item_count
+    
+    def design_new_table(self):
+        new_bucket_size = NONEED
+        if self.item_count > self.bucket_size*0.7:
+            new_bucket_size = (self.bucket_size)*2+1
+        elif self.item_count <  self.bucket_size*0.3:
+            new_bucket_size = (self.bucket_size//4)*2+1
+        return  new_bucket_size
+        
 
 
 # Test the functional behavior of the hash table.
@@ -145,7 +170,6 @@ def functional_test():
     assert hash_table.put("aaa", 1) == True
     assert hash_table.get("aaa") == (1, True)
     assert hash_table.size() == 1
-
     assert hash_table.put("bbb", 2) == True
     assert hash_table.put("ccc", 3) == True
     assert hash_table.put("ddd", 4) == True
@@ -161,9 +185,8 @@ def functional_test():
     assert hash_table.put("aaa", 11) == False
     assert hash_table.get("aaa") == (11, True)
     assert hash_table.size() == 4
-    # hash_table.print_table()
+    
     assert hash_table.delete("aaa") == True
-    # hash_table.print_table()
     assert hash_table.get("aaa") == (None, False)
     assert hash_table.size() == 3
 
@@ -194,7 +217,6 @@ def functional_test():
     assert hash_table.get("cab") == (5, True)
     assert hash_table.get("cba") == (6, True)
     assert hash_table.size() == 6
-
     assert hash_table.delete("abc") == True
     assert hash_table.delete("cba") == True
     assert hash_table.delete("bac") == True
