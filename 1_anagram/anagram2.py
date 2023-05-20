22763bb26109aa666405526f844318453060a31c
ss-cosmos-hue <122190026+ss-cosmos-hue@users.noreply.github.com>
2023-05-20 12:41:08 +0900

    implement  anagram2  with prime num hash

--- 1_anagram/anagram2.py
+++ 1_anagram/anagram2.py
@@ -1,31 +1,25 @@
 import sys
 SCORES = [1, 3, 2, 2, 1, 3, 3, 1, 1, 4, 4, 2, 2, 1, 1, 3, 4, 1, 1, 1, 2, 3, 3, 4, 3, 4]
+HASHES = [2, 3, 5, 7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101]
 NUM_ALPHABET = 26
 INVALID = -1
 
 class WordInfo():
     def __init__(self,raw_word) -> None:
         self.anagram = raw_word 
-        self.composition = self.breakdown(raw_word)
-        self.score = self.eval(self.composition)
-    
-    def breakdown(self, word):
-        composition = [0]* NUM_ALPHABET#0,1,2,...,25
+        self.score,self.hash = self.calc_hash_score(raw_word)    
+
+    def calc_hash_score(self, word):
+        score = 0
+        hash = 1
         for letter in word:
             idx = letter2idx(letter)
             if (idx!= INVALID):
-                composition[idx] += 1
+                hash *= HASHES[idx]
+                score +=  SCORES[idx] 
             else:
-                return INVALID
-        return composition
-    
-    def eval(self,composition):
-        score = 0
-        if composition != INVALID:
-            for i in range(NUM_ALPHABET):
-                score += composition[i]*SCORES[i]
-            return score
-        return INVALID
+                score, hash = INVALID,INVALID
+        return score,hash
 
 def letter2idx(letter):
     if 0 <= ord(letter)-ord('a')  <  NUM_ALPHABET:
@@ -38,15 +32,10 @@ def readwords(filename):
         words = f.read().split("\n")
     return words
     
-def generatable(seed,target_seed):
-    #return if seed can be generated from target_seed
-    for i in range(len(seed)):
-        if seed[i]<=target_seed[i]:
-            pass
-        else:
-            return False 
-    return True
-        
+def generatable(hash,target_hash):
+    #return if seed can be generated from target
+    return (hash != INVALID) and  (target_hash != INVALID) and  (target_hash % hash == 0)
+
 def anagram2(target_seed,descending_wordinfo_list):
     """_summary_
 
@@ -61,29 +50,28 @@ def anagram2(target_seed,descending_wordinfo_list):
     ans = None
     
     for wordinfo in descending_wordinfo_list:
-        if generatable(wordinfo.composition,target_seed):
+        if generatable(wordinfo.hash,target_seed):
             ans = wordinfo.anagram
             break
     return ans
 
 def main(datafile,answerfile):
     raw_words = readwords('words.txt')
-    target_words = readwords(datafile)
+    target_words = readwords(datafile)   
     wordinfo_list = [WordInfo(raw_word) for raw_word in raw_words]
     descending_wordinfo_list = sorted(wordinfo_list, key = lambda wordinfo: wordinfo.score, reverse=True)
 
     with open(answerfile,'wt') as f:
         for (i,target) in enumerate(target_words):
-            target_seed = WordInfo(target).composition
+            target_hash = WordInfo(target).hash
             ans = ""
-            if len(target) == 0 or target_seed == -1:
+            if len(target) == 0 or target_hash == INVALID:
                 ans = ""
             else:
-                ans = anagram2(target_seed,descending_wordinfo_list)
+                ans = anagram2(target_hash,descending_wordinfo_list)
             f.write(ans+"\n")
             if i%50 == 0:
                 print(i)
-
     return
 
 if __name__ == "__main__":
